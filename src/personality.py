92810291bf4e98c66dfe25e514d66cdeb6ed7abf
japser36 <jasper@looman.ca>
2019-01-26 20:54:49 -0800

    Mack is alive! Added two files, mack.py and personality.py Implemented basic call and response when you run mack.py in a command prompt using 'python mack.py'

---
+++ src/personality.py
@@ -0,0 +1,10 @@
+print("loading personality...")
+intents = dict()
+
+intents['getState'] = "I'm good, thanks!"
+intents['getPreference'] = "Yes, I like that."
+intents['getContinuousState'] = "Yeah, I do that."
+intents['exit'] = "Good bye!"
+intents['greeting'] = "Hi!"
+
+print("Personality loaded")
